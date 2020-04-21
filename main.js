d49edf081a0aee6de1e7216c39a95f3e7f20a498
AndreCatarinoUA <andrealmeida.catarino123@gmail.com>
2020-04-21 04:05:44 +0100

    fileSystem IO Implementation

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+"use strict";
+
+require("electron-reload")(__dirname);
+
 // Modules to control application life and create native browser window
 
-const {app, BrowserWindow} = require("electron");
-const path = require("path");
+const {app, BrowserWindow} = require('electron')
+const path = require('path');
+const { electron } = require('process');
 
+// require ('electron-reload') (__dirname);
 
 
 
@@ -12,7 +18,8 @@ function createWindow () {
     width: 1920,
     height: 1080,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js")
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true
     },
   });
 
@@ -28,35 +35,31 @@ function createWindow () {
   childWindow.show();
   });
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-}
 
+// Open the DevTools.
+mainWindow.webContents.openDevTools();
+}
 
 
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow)
 
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function () {
+app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+  if (process.platform !== 'darwin') app.quit()
+})
 
-app.on("activate", function () {
+app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if(BrowserWindow.getAllWindows().length === 0){
-    createWindow();
-  }
-});
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
