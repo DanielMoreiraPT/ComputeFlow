3ee465f8a6ea8b9d7e34b6e09bcb277420dbb3c7
Rui Melo <44201826+rufimelo99@users.noreply.github.com>
2020-04-29 21:50:53 +0100

    Add files via upload

---
+++ jsonRelated.js
@@ -0,0 +1,14 @@
+//JSON read file function
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+  }
+
+
