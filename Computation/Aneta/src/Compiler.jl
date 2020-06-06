95537e23458b735e121b43b93392005a05b2f29b
anunia <pawelec.aneta98@gmail.com>
2020-06-06 18:10:36 +0100

    Compiler

---
+++ Computation/Aneta/src/Compiler.jl
@@ -0,0 +1,53 @@
+"""
+# module Run2
+
+- Julia version:
+- Author: anunia
+- Date: 2020-04-26
+
+# Examples
+
+```jldoctest
+julia>
+```
+"""
+module Compiler
+    import Base.Threads
+    import Distributed
+    include("JsonReader.jl")
+
+
+    modules = JsonReader.upload_modules("Computation/Aneta/ToUppercase_test.json")
+
+    modules_dict = Dict()
+    modules_info = Dict()
+    tasks = []
+
+    for m in modules
+        include(m.functionid*".jl")
+        in_channels = Dict()
+        out_channels = Dict()
+
+            for io in m.io.outputs
+                out_channels[io.port_id] = io.channel
+            end
+            for connection in m.connections.inputs
+                module_in = connection.module_id
+                module_port = connection.module_port
+                input_port = connection.input_port
+                in_channels[input_port] = modules[module_in].io.outputs[module_port].channel
+            end
+        modules_dict = Dict("functionid" => m.functionid,"input_channels" => in_channels, "output_channels" => out_channels)
+        modules_info[m.id] = modules_dict
+        string = """$(m.functionid).$(m.functionid)_f(modules_info[$(m.id)]["input_channels"], modules_info[$(m.id)]["output_channels"])"""
+        push!(tasks,@task (eval(Meta.parse(string))))
+
+    end
+    for task in tasks
+        schedule( task)
+    end
+
+    println("Im running")
+
+
+end
